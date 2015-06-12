7fe74cd6784358d6d7e05bb2600462d04212e1f3
tardismeg <aaron@aaroncalder.com>
2015-06-11 21:11:29 -0600

    Added source code for example server from class.

---
+++ src/chat/ConnectionServer.java
@@ -0,0 +1,42 @@
+package chat_client;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ConnectionServer implements Runnable {
+	
+	@Override
+	public void run() {
+		try {
+			ServerSocket server = new ServerSocket(8989);
+			Socket clientConnection = server.accept();
+
+			Scanner input = new Scanner(clientConnection.getInputStream());
+			String chatMessage;
+			while (clientConnection.isConnected() && !clientConnection.isClosed()) {
+				chatMessage = input.next();
+				System.out.println(chatMessage);
+
+				if (chatMessage.equals("exit")) {
+					clientConnection.close();
+					break;
+				}
+			}
+
+			server.close();
+			input.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Runnable server = new ConnectionServer();
+		Thread thread = new Thread(server);
+		thread.start();
+		
+		System.out.println("DONE");
+	}
+}
