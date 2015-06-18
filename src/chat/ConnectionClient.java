12e0a0b5c5379b4bc4c02b5b1e51a9cf4fe5542c
mat brewer <mathewbrewer@mail.weber.edu>
2015-06-17 21:47:18 -0600

    more code

--- src/chat/ConnectionClient.java
+++ src/chat/ConnectionClient.java
@@ -3,6 +3,8 @@ package chat;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,13 +13,17 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class ConnectionClient implements Runnable
 {
-    protected BufferedReader input;
-    protected BufferedWriter serverMessage;
+    //protected BufferedReader input;
+   // protected BufferedWriter clientMessage;
     protected Socket serverConnection;
+    public clientFrame cFrame;
+    public DataOutputStream output;
+    public DataInputStream input;
     
-     public ConnectionClient()
+    public ConnectionClient()
     {
     
         //server contruct
@@ -31,10 +37,42 @@ public class ConnectionClient implements Runnable
         
         try 
         {
+            
             serverConnection = new Socket("localhost",8989);
-               
-            this.input = new BufferedReader (new InputStreamReader(serverConnection.getInputStream()));
-                      
+            
+            
+            //input = new BufferedReader (new InputStreamReader(serverConnection.getInputStream()));
+            //clientMessage = new BufferedWriter(new OutputStreamWriter(serverConnection.getOutputStream()));   
+            this.input = new DataInputStream(serverConnection.getInputStream());
+            this.output = new DataOutputStream(serverConnection.getOutputStream());   
+            while(serverConnection.isConnected() && !serverConnection.isClosed())
+            {
+
+                try
+                {
+
+                    String incomingMessage = input.readUTF();//read message from server
+
+
+                    if (incomingMessage.equals("exit")) 
+                    {
+                        serverConnection.close();
+                        break;
+                    }
+                    else
+                    {
+                       cFrame.addTextToWindow(incomingMessage);//send to method in client frame to append to chatoutput
+                    }
+
+                }
+                catch(NullPointerException e)
+                {
+
+                }
+
+            }    
+            
+            
         }        
                             
         catch (NullPointerException e)
@@ -60,4 +98,18 @@ public class ConnectionClient implements Runnable
 
         }
     }
+    public void messageToServer(String message) throws IOException
+    {
+        try
+        {
+            
+            output.writeUTF(message);//send message out to server
+            
+        }
+        catch(IOException e)
+        {
+            throw e;
+        }
+    }
+    
 }
